9df6cc9c0a9c921d7302bd05ed05a7c57653ce60
Sekhar Paladugu <sekharp@gmail.com>
2016-01-11 20:22:59 -0700

    styling more complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,9 @@
 class BooksController < ApplicationController
   def index
+    @book = service_connection.client.book('OL7730286M')
   end
 
   def show
+    @book = service_connection.client.book('OL7730286M')
   end
 end
