f0fca131e79251949a6dbd93a84ea915c6ce1be4
Sekhar Paladugu <sekharp@gmail.com>
2016-01-12 15:00:57 -0700

    before moving on to handroll api

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,11 @@
 class BooksController < ApplicationController
   def index
     @book = service_connection.client.book('OL7730286M')
+    @book_two = service_connection.client.book('OL1565969M')
   end
 
   def show
+    isbn =
     @book = service_connection.client.book('OL7730286M')
   end
 end
