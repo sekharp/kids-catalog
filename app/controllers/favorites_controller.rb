88d62135de047b376ad7decf1b71427922ce6732
Sekhar Paladugu <sekharp@gmail.com>
2016-01-21 10:35:44 -0700

    small fixes

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,16 @@
 class FavoritesController < ApplicationController
-  #note to fix create for creating with mark as read first time
   def create
-    UserBook.create(book_id: params[:book_id], user_id: params[:user_id], favorited: params[:favorited])
-    book = Book.find(params[:book_id])
-    redirect_to book_path(id: book.id)
-    flash[:success] = "Successfully marked as favorite the book #{book.title}!"
+    if params[:favorited]
+      UserBook.create(book_id: params[:book_id], user_id: params[:user_id], favorited: params[:favorited])
+      book = Book.find(params[:book_id])
+      redirect_to book_path(id: book.id)
+      flash[:success] = "Successfully marked as favorite the book #{book.title}!"
+    else
+      UserBook.create(book_id: params[:book_id], user_id: params[:user_id], read: params[:read])
+      book = Book.find(params[:book_id])
+      redirect_to book_path(id: book.id)
+      flash[:success] = "Successfully marked as read the book #{book.title}!"
+    end
   end
 
   def update
@@ -12,13 +18,13 @@ class FavoritesController < ApplicationController
     user_book = UserBook.where(book_id: params[:book_id], user_id: params[:user_id]).first
     if params[:favorited] == "true"
       user_book.update(:favorited => true)
-      flash[:success] = "Successfully added favorite status for the book #{book.title}."
+      flash[:success] = "Successfully marked as favorite the book #{book.title}."
     elsif params[:favorited] == "false"
       user_book.update(:favorited => false)
       flash[:success] = "Successfully removed favorite status for the book #{book.title}."
     elsif params[:read] == "true"
       user_book.update(:read => true)
-      flash[:success] = "Successfully added mark as read status for the book #{book.title}."
+      flash[:success] = "Successfully marked as read the book #{book.title}."
     else
       user_book.update(:read => false)
       flash[:success] = "Successfully removed mark as read status for the book #{book.title}."
--- test/integration/visitor_looks_at_books_test.rb
+++ test/integration/visitor_looks_at_books_test.rb
@@ -6,13 +6,6 @@ class VisitorLooksAtBooksTest < ActionDispatch::IntegrationTest
   def setup
     Capybara.app = KidsCatalog::Application
     @book = Book.create(title: "A book", author_first_name: "Sekhar", author_last_name: "Paladugu", isbn: "0689857578")
-    # stub_request(:get, "localhost:3000/books/#{@book.id}")
-    # stub_request(:get, "http://covers.openlibrary.org/b/isbn/0689857578-L.jpg").
-    #   with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Hurley v0.2'}).
-    #   to_return(:status => 200, :body => "", :headers => {})
-    # stub_request(:get, "https://www.googleapis.com/books/v1/volumes?maxResults=5&q=0689857578&startIndex=0").
-    #   with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
-    #   to_return(:status => 200, :body => "", :headers => {})
   end
 
   test "visitor can see login link" do
